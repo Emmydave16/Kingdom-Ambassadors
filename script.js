fc2852ad46f3a4181ee935a0d168c63787078df4
Emmydave16 <davidanjolaoluwaemmanuel16@gmail.com>
2025-09-18 11:48:17 +0100

    new

--- script.js
+++ script.js
@@ -92,3 +92,14 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     })();
 });
+
+feather.replace();
+
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+if (menuBtn) {
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+}
