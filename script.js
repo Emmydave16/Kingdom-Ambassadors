d74e3a20174099b52334237ca4af5c369c6de29a
Emmydave16 <davidanjolaoluwaemmanuel16@gmail.com>
2025-09-18 11:49:10 +0100

    script

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ const menuBtn = document.getElementById("menu-btn");
 const mobileMenu = document.getElementById("mobile-menu");
 
 if (menuBtn) {
-  menuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-  });
+    menuBtn.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
 }
